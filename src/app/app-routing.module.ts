193a4817cd7364e22ddfa4a9737057e00cbcbf48
Thanh Nguyen <thanh.nguyen@teamrg.com>
2023-04-03 10:59:41 -0400

    add component showcasing primeng usage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GettingStartedComponent } from './components/getting-started/getting-started.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: GettingStartedComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
